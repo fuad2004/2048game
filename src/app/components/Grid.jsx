896fdba47b15e2eec758fc908c43e57552e4cffe
Fuad Damirchiev <fuad.damirchiyev@azerimed.com>
2024-07-15 10:00:55 +0400

    game over

--- src/app/components/Grid.jsx
+++ src/app/components/Grid.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 
 const Grid = ({ arrFromProps }) => {
   const [arr, setArr] = useState([...arrFromProps]);
+  const [isGameOver, setIsGameOver] = useState(false);
+
   function getRowLeft(arr) {
     let temp = -2;
     arr.forEach((_, index) => {
@@ -126,7 +128,8 @@ const Grid = ({ arrFromProps }) => {
     });
     const emptyTilesLength = emptyTiles.length;
     if (emptyTilesLength != 0) {
-      const randomTile = emptyTiles[Math.floor(Math.random() * emptyTilesLength)];
+      const randomTile =
+        emptyTiles[Math.floor(Math.random() * emptyTilesLength)];
       if (Math.floor(Math.random() * 10) == 9) {
         arr[randomTile[0]][randomTile[1]] = 4;
       } else {
@@ -144,18 +147,60 @@ const Grid = ({ arrFromProps }) => {
     let newArr = setNewTile(arr);
     let newArrV2 = setNewTile(newArr);
     setArr(newArrV2);
+    setIsGameOver(false);
+  }
+
+  function isGameOverFunc() {
+    setArr((arr) => {
+      let isFull = true;
+
+      arr.forEach((row) => {
+        row.forEach((item) => {
+          if (item == 0) {
+            isFull = false;
+          }
+        });
+      });
+
+      let isSimilar = true;
+      arr.forEach((row, rowIndex) => {
+        row.forEach((_, colIndex) => {
+          if (
+            arr[rowIndex][colIndex + 1] != undefined &&
+            arr[rowIndex][colIndex] == arr[rowIndex][colIndex + 1]
+          ) {
+            isSimilar = false;
+          } else if (
+            arr.length >= rowIndex + 2 &&
+            arr[rowIndex][colIndex] == arr[rowIndex + 1][colIndex]
+          ) {
+            isSimilar = false;
+          }
+        });
+      });
+
+      if (isSimilar && isFull) {
+        setIsGameOver(true);
+      }
+
+      return arr;
+    });
   }
 
   useEffect(() => {
     function keyDownEvent(e) {
       if (e.code == "ArrowUp") {
         goTop();
+        isGameOverFunc();
       } else if (e.code == "ArrowDown") {
         goBottom();
+        isGameOverFunc();
       } else if (e.code == "ArrowRight") {
         goRight();
+        isGameOverFunc();
       } else if (e.code == "ArrowLeft") {
         goLeft();
+        isGameOverFunc();
       } else if (e.code == "KeyR") {
         restartGame();
       }
@@ -168,78 +213,95 @@ const Grid = ({ arrFromProps }) => {
   }, [arr]);
 
   return (
-    <div className="h-screen grid place-items-center container mx-auto">
-      <div className="bg-[#bbada0] rounded-md grid grid-cols-4 grid-rows-4 p-4 gap-4">
-        {arr.map((row, rowIndex) => {
-          return row.map((item, index) => {
-            let backgroundColor = "#eee4da59";
-            let color = "#776e65";
-            let fontSize = "48px";
-            switch (item) {
-              case 2:
-                backgroundColor = "#eee4da";
-                break;
-              case 4:
-                backgroundColor = "#eee1c9";
-                break;
-              case 8:
-                backgroundColor = "#f3b27a";
-                color = "#f9f6f2";
-                break;
-              case 16:
-                backgroundColor = "#f69664";
-                color = "#f9f6f2";
-                fontSize = "44px";
-                break;
-              case 32:
-                backgroundColor = "#f77c5f";
-                color = "#f9f6f2";
-                fontSize = "44px";
-                break;
-              case 64:
-                backgroundColor = "#f75f3b";
-                color = "#f9f6f2";
-                fontSize = "44px";
-                break;
-              case 128:
-                backgroundColor = "#edd073";
-                color = "#f9f6f2";
-                fontSize = "40px";
-                break;
-              case 256:
-                backgroundColor = "#edd073";
-                color = "#f9f6f2";
-                fontSize = "40px";
-                break;
-              case 512:
-                backgroundColor = "#edd073";
-                color = "#f9f6f2";
-                fontSize = "40px";
-                break;
-              default:
-                break;
-            }
-            if (item != 0) {
-              return (
-                <div
-                  key={rowIndex + "" + index}
-                  style={{ backgroundColor, color, fontSize }}
-                  className="w-24 h-24 rounded grid place-items-center font-bold">
-                  {item}
-                </div>
-              );
-            } else {
-              return (
-                <div
-                  key={rowIndex + "" + index}
-                  style={{ backgroundColor, color, fontSize }}
-                  className="w-24 h-24 rounded grid place-items-center font-bold"></div>
-              );
-            }
-          });
-        })}
+    <>
+      <div className="h-screen grid place-items-center container mx-auto">
+        <div className="bg-[#bbada0] relative rounded-md grid grid-cols-4 grid-rows-4 p-4 gap-4">
+          {isGameOver && (
+            <div className="gameOver absolute top-0 bg-white/50 left-0 w-full h-full grid place-items-center">
+              <div className="flex flex-col items-center gap-4">
+                <div className="text-6xl font-semibold">Game Over!</div>
+                <button
+                  onClick={restartGame}
+                  className="text-xl text-white w-fit hover:bg-[#776555] bg-[#8f7a66] font-semibold rounded py-1 px-4"
+                >
+                  Try again
+                </button>
+              </div>
+            </div>
+          )}
+          {arr.map((row, rowIndex) => {
+            return row.map((item, index) => {
+              let backgroundColor = "#eee4da59";
+              let color = "#776e65";
+              let fontSize = "48px";
+              switch (item) {
+                case 2:
+                  backgroundColor = "#eee4da";
+                  break;
+                case 4:
+                  backgroundColor = "#eee1c9";
+                  break;
+                case 8:
+                  backgroundColor = "#f3b27a";
+                  color = "#f9f6f2";
+                  break;
+                case 16:
+                  backgroundColor = "#f69664";
+                  color = "#f9f6f2";
+                  fontSize = "44px";
+                  break;
+                case 32:
+                  backgroundColor = "#f77c5f";
+                  color = "#f9f6f2";
+                  fontSize = "44px";
+                  break;
+                case 64:
+                  backgroundColor = "#f75f3b";
+                  color = "#f9f6f2";
+                  fontSize = "44px";
+                  break;
+                case 128:
+                  backgroundColor = "#edd073";
+                  color = "#f9f6f2";
+                  fontSize = "40px";
+                  break;
+                case 256:
+                  backgroundColor = "#edd073";
+                  color = "#f9f6f2";
+                  fontSize = "40px";
+                  break;
+                case 512:
+                  backgroundColor = "#edd073";
+                  color = "#f9f6f2";
+                  fontSize = "40px";
+                  break;
+                default:
+                  break;
+              }
+              if (item != 0) {
+                return (
+                  <div
+                    key={rowIndex + "" + index}
+                    style={{ backgroundColor, color, fontSize }}
+                    className="w-24 h-24 rounded grid place-items-center font-bold"
+                  >
+                    {item}
+                  </div>
+                );
+              } else {
+                return (
+                  <div
+                    key={rowIndex + "" + index}
+                    style={{ backgroundColor, color, fontSize }}
+                    className="w-24 h-24 rounded grid place-items-center font-bold"
+                  ></div>
+                );
+              }
+            });
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
